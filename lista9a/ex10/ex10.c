4142130a2e80e4784bb9acd0fb9c1c5b520a38cf
tmildemberger <thimil@alunos.utfpr.edu.br>
2019-06-04 23:31:41 -0300

    mais coisa

--- lista9a/ex10/ex10.c
+++ lista9a/ex10/ex10.c
@@ -6,8 +6,18 @@
 
 int main() {
     char str[N];
+    // int tam;
+    // scanf("%d", &tam);
     scanf("%s", str);
 
-    if ((int)sqrt(strlen(str)))
+    // if ((int)sqrt(strlen(str)))
+    int i;
+    for (i = 0; i < (int)sqrt(strlen(str)); ++i) {
+        int j;
+        for (j = 0; j < (int)sqrt(strlen(str)); ++j) {
+            putchar(str[i + j * (int)sqrt(strlen(str))]);
+        }
+    }
+    putchar('\n');
     return 0;
 }
